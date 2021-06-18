812583716b7fa35e1d6b31e92c395b177e1e7dae
sugyeong jo <ssuya1020@gmail.com>
2021-06-18 13:20:41 +0900

    skills upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+var $desc = $('.tab__desc');
+        var $tabMenu = $('.tab__menu a');
+        
+        $desc.hide();
+        $desc.eq(0).show();
+
+        $tabMenu.click(function(){
+
+            var idx = $(this).index();
+            $tabMenu.removeClass('selected');
+            $(this).addClass('selected');
+            $desc.hide();
+            $desc.eq(idx).slideDown();
+        });
