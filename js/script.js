7d898a4cd9064230ad557dc2bca862009658e51c
josugyeong <84550603+josugyeong@users.noreply.github.com>
2021-06-19 17:49:35 +0900

    skill ~ing

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 'use strict';
 
 var $desc = $('.tab__desc');
-        var $tabMenu = $('.tab__menu a');
+        var $tabMenu = $('.tab__menu button');
         
+        // 초기 세팅
         $desc.hide();
         $desc.eq(0).show();
 
+        // tab 메뉴를 클릭하면 해당하는 desc를 보여줘
         $tabMenu.click(function(){
 
             var idx = $(this).index();
+            // console.log(idx);
             $tabMenu.removeClass('selected');
             $(this).addClass('selected');
             $desc.hide();
             $desc.eq(idx).slideDown();
+            
         });
