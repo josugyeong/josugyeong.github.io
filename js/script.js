c0b033a3e904d6a62f2b7ad40084a79d7d586cc0
sugyeong jo <ssuya1020@gmail.com>
2021-06-29 11:18:43 +0900

    design modal upload

--- js/script.js
+++ js/script.js
@@ -167,7 +167,6 @@ var $modal = $('.modal');
         $btn.click(function(){
             var imgSrc = $(this).attr('data-img');
             console.log(imgSrc)
-            // var imgSrc = "";
             $modalImg.attr('src', imgSrc);
             $overlay.fadeIn();
         })
@@ -180,14 +179,6 @@ var $modal = $('.modal');
             }
         });
 
-        // $('.slick-container').slick();
-       /*  $('.slick-container').slick({
-                infinite: true, //양방향 무한 모션
-                speed:300, // 슬라이드 스피드
-                autoplay: true, //자동플레이 유무( false시 자동플레이 안됨 )
-                autoplaySpeed:4000, // 자동플레이 스피드
-                slidesToShow: 3
-              }); */
               $('.slick-container').slick({
                 infinite: true, //양방향 무한 모션
                 speed:300, // 슬라이드 스피드
@@ -196,13 +187,6 @@ var $modal = $('.modal');
                 slidesToShow: 3, //한 화면에 보여줄 아이템수
                 slidesToScroll: 3 // 한번에 슬라이드 시킬 아이템 개수
               });
-/*               $('.slick-container').slick({
-                infinite: true,
-                speed: 300,
-                slidesToShow: 1, // 1개씩 슬라이드 
-                centerMode: true,  // 중앙배치
-                variableWidth: true // width 가 각각 다름을 표시 
-              }); */
               $(function(){
                 // 이미지를 클릭했을때
                 var $portfolioImg = $('.portfolio__img');
